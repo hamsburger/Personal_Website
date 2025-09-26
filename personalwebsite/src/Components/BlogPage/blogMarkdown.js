dc4d3b2f103877ca34d59dd51cb06fc1afc68a8b
hamsburger <harriszheng222@gmail.com>
2025-09-26 00:56:09 -0700

    replace both \n and \n with space folloewd by \n

--- personalwebsite/src/Components/BlogPage/blogMarkdown.js
+++ personalwebsite/src/Components/BlogPage/blogMarkdown.js
@@ -48,9 +48,9 @@ const BlogMarkdown = ({ content }) => {
           return res.text()
         })
         .then(text => {
-          text = text.slice(16, -2) // Remove export default
-          text = text.replace(/\\r\\n|\\n\\n/g, " \n") // Replace \r\n with two spaces and \n
           console.log(text)
+          text = text.slice(16, -2) // Remove export default
+          text = text.replace(/\\r\\n|\\n\\n|\n+/g, " \n") // Replace \r\n with two spaces and \n
           setRenderedMD(text)
         })
     },
